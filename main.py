f54e1c1c1582351a4dc4e4e77a83beb5908f8c62
912733117 <72393802+912733117@users.noreply.github.com>
2020-12-03 23:43:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 token=os.environ['PLUS_KEY']
+
 plant1=os.environ['PLANT1']
 fruit1=os.environ['FRUIT1']
 pet1=os.environ['PET1']
+dream_factory1=os.environ['DREAM_FACTORY1']
+jd_factory1=os.environ['JD_FACTORY1']
+
+
 plant2=os.environ['PLANT2']
 fruit2=os.environ['FRUIT2']
 pet2=os.environ['PET2']
-def jd_subscribe(plant_code,fruit_code,pet_code):
+dream_factory2=os.environ['DREAM_FACTORY2']
+jd_factory2=os.environ['JD_FACTORY2']
+
+def jd_subscribe(plant_code,fruit_code,pet_code,dream_factory_code,jd_factory_code):
     try:
         # 模拟浏览器打开网站
         chrome_options = Options()
@@ -20,11 +28,17 @@ def jd_subscribe(plant_code,fruit_code,pet_code):
         plant_code_url = "http://api.turinglabs.net/api/v1/jd/bean/create/"+str(plant_code)+"/"
         fruit_code_url = "http://api.turinglabs.net/api/v1/jd/farm/create/"+str(fruit_code)+"/"
         pet_code_url = "http://api.turinglabs.net/api/v1/jd/pet/create/"+str(pet_code)+"/"
+        dream_factory_url = "http://api.turinglabs.net/api/v1/jd/jxfactory/create/"+str(dream_factory_code)+"/"
+        jd_factory_url = "http://api.turinglabs.net/api/v1/jd/ddfactory/create/"+str(jd_factory_code)+"/"
         driver.get(plant_code_url)
         driver.implicitly_wait(100)
         driver.get(fruit_code_url)
         driver.implicitly_wait(100)
         driver.get(pet_code_url)
+        driver.implicitly_wait(100)
+        driver.get(dream_factory_url)
+        driver.implicitly_wait(100)
+        driver.get(jd_factory_url)
     except:
         driver.quit()
         print("上车失败!")
@@ -40,5 +54,5 @@ def jd_subscribe(plant_code,fruit_code,pet_code):
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
 if __name__ == "__main__":
-   jd_subscribe(plant1,fruit1,pet1)
-   jd_subscribe(plant2,fruit2,pet2)
+   jd_subscribe(plant1,fruit1,pet1,dream_factory1,jd_factory1)
+   jd_subscribe(plant2,fruit2,pet2,dream_factory2,jd_factory2)
